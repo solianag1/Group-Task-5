88bcde8d6993f9bd009befbee42ea130e14a7029
Soliana Getachew <solianag1@gmail.com>
2025-06-30 09:40:54 -0400

    Automate Docker build and push

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,4 +1,12 @@
 //Soliana Getachew-sgetach1@umbc.edu
+package HelloWorld;
+
+/**
+ * Project: IS247 Group Project
+ * Author: Soliana Getachew
+ * Version: 1.0
+ * Description: This program prints a simple message using Docker.
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Docker and Java!");
